aab7ef23150e1de530255d739fe05c41ec926744
gevontutmazian8 <joacoutmazian@gmail.com>
2025-08-16 20:33:43 -0300

    Update Ejercicio1.java
    ejercicio1

--- ejercicios/Ejercicio1.java
+++ ejercicios/Ejercicio1.java
@@ -1,1 +1,13 @@
+package ejercicios;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        double valor = 9.7;
+
+        // Casting explícito: double a int
+        int numero = (int) valor;
+        System.out.println("Valor original (double): " + valor);
+        System.out.println("Valor convertido (int): " + numero);
+    }
+}
 
