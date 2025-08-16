e6c324659595675c2de255d020eeed6e5f802c43
gevontutmazian8 <joacoutmazian@gmail.com>
2025-08-16 20:36:37 -0300

    Update Ejercicio8.java

--- ejercicios/Ejercicio8.java
+++ ejercicios/Ejercicio8.java
@@ -1,1 +1,18 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese un número entero: ");
+        int a = input.nextInt();
+        System.out.print("Ingrese otro número entero: ");
+        int b = input.nextInt();
+
+        System.out.println("División entera: " + (a / b));
+        System.out.println("División con decimales: " + ((double) a / b)); // casting
+    }
+}
 
