e42465963c432f4e6562322fd7374997e71ba193
gevontutmazian8 <joacoutmazian@gmail.com>
2025-08-16 20:34:00 -0300

    Update Ejercicio10.java
    ejercicio2

--- ejercicios/Ejercicio10.java
+++ ejercicios/Ejercicio10.java
@@ -1,1 +1,20 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese su edad: ");
+        int edad = input.nextInt();
+
+        input.nextLine(); // limpiar buffer
+
+        System.out.print("Ingrese su nombre: ");
+        String nombre = input.nextLine();
+
+        System.out.println("Hola " + nombre + ", tenés " + edad + " años.");
+    }
+}
 
