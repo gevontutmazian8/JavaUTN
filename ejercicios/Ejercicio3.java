ddf33342be2601297ebc2fcd9aa3c7f1d21db305
gevontutmazian8 <joacoutmazian@gmail.com>
2025-08-16 20:35:21 -0300

    Update Ejercicio3.java

--- ejercicios/Ejercicio3.java
+++ ejercicios/Ejercicio3.java
@@ -1,1 +1,19 @@
+package ejercicios;
 
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese un número entero: ");
+        int num1 = input.nextInt();
+        System.out.print("Ingrese otro número entero: ");
+        int num2 = input.nextInt();
+
+        System.out.println("Suma: " + (num1 + num2));
+        System.out.println("Resta: " + (num1 - num2));
+        System.out.println("Multiplicación: " + (num1 * num2));
+        System.out.println("División: " + ((double)num1 / num2)); // casting para decimales
+    }
+}
