e81dd966d82ad8c2be234185c74e194bed613a74
gevontutmazian8 <joacoutmazian@gmail.com>
2025-08-16 20:35:36 -0300

    Update Ejercicio4.java

--- ejercicios/Ejercicio4.java
+++ ejercicios/Ejercicio4.java
@@ -1,1 +1,15 @@
+package ejercicios;
 
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        String nombre = "Juan Pérez";
+        int edad = 30;
+        double altura = 1.75;
+        boolean estudiante = true;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Estudiante: " + estudiante);
+    }
+}
