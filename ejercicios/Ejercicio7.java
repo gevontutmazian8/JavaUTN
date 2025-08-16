45b4b0b8a237b74c381e84ec775680be6c6e292f
gevontutmazian8 <joacoutmazian@gmail.com>
2025-08-16 20:36:26 -0300

    Update Ejercicio7.java

--- ejercicios/Ejercicio7.java
+++ ejercicios/Ejercicio7.java
@@ -1,1 +1,13 @@
+package ejercicios;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        int x = 10;          // Línea 1: declaración e inicialización (instrucción)
+        x = x + 5;           // Línea 2: asignación (instrucción)
+        System.out.println(x); // Línea 3: llamada a método (instrucción)
+
+        // Expresión: x + 5
+        // Instrucción: la línea completa que realiza la acción
+    }
+}
 
