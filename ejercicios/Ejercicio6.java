7b3ede824b38cc296e1c364ebac09012fe61bca9
gevontutmazian8 <joacoutmazian@gmail.com>
2025-08-16 20:36:14 -0300

    Update Ejercicio6.java

--- ejercicios/Ejercicio6.java
+++ ejercicios/Ejercicio6.java
@@ -1,1 +1,10 @@
+package ejercicios;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Pérez");
+        System.out.println("Edad: 30 años");
+        System.out.println("Dirección: \"Calle Falsa 123\"");
+    }
+}
 
