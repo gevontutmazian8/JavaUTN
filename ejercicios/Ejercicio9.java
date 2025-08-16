6ee325241bf4a86288471f998f2ff1ffd4088948
gevontutmazian8 <joacoutmazian@gmail.com>
2025-08-16 20:36:54 -0300

    Update Ejercicio9.java

--- ejercicios/Ejercicio9.java
+++ ejercicios/Ejercicio9.java
@@ -1,1 +1,14 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = scanner.nextLine(); // CORRECCIÓN: antes estaba scanner.nextInt()
+        System.out.println("Hola, " + nombre);
+    }
+}
 
